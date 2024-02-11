f78b474d22b453dea12b7f5647ac2c7736ac9967
diankartiko <auliakartiko28@gmail.com>
2024-02-11 13:08:13 +0700

    update portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./public/***.{html,js}/'],
+    content: ['index.html'],
     darkMode: 'class',
     theme: {
-        container : {
-            center:'true',
-            padding : '16px', 
+        container: {
+            center: 'true',
+            padding: '16px',
         },
         extend: {
             colors: {
                 primary: '#14b8a6',
                 dark: '#0f172a',
                 hover: '#2dd4bf',
-                secondary : '#64748b',
+                secondary: '#64748b',
             },
-            screen:{
-                '2xl' : '1320px'
-            }
+            screen: {
+                '2xl': '1320px',
+            },
         },
     },
     plugins: [],
